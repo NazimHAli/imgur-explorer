f73d7def26b77f2582f6078c9c55a904d193053c
Nazim <nazimali@gmail.com>
2021-10-14 21:56:31 -0400

    REF: Update TW config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   purge: {
     enabled: true,
-    content: ["./src/*.html", "./src/**/*.ts", "./src/**/*.tsx"],
+    content: ["./*.html", "./src/**/*.ts", "./src/**/*.tsx"],
   },
   darkMode: false, // or 'media' or 'class'
   theme: {
