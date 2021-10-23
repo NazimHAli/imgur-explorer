506850cf1352ada20d9f1a3b55134cc961ea2590
Nazim <nazimali@gmail.com>
2021-10-22 22:26:00 -0400

    REF: Save new files

---
+++ src/utils/state.ts
@@ -0,0 +1,173 @@
+import { getSelectedItem } from "@/utils/dataUtils";
+import { State, Action } from "@/utils/types";
+
+const initialState = {
+  finishedLazyLoading: false,
+  galleryTags: {},
+  isLoading: false,
+  items: [],
+  requestArgs: {
+    filter: true,
+    method: "search",
+    newSearch: true,
+    page: 1,
+    query: "meow",
+    selectedItemID: "",
+    sort: "viral",
+    tagName: "",
+    window: "all",
+  },
+  requestError: false,
+  selectedItem: {},
+  selectedItemComments: [],
+  selectedTag: {},
+};
+
+function setItems(state: State, action: Action): State {
+  return {
+    ...state,
+    finishedLazyLoading: action?.finishedLazyLoading
+      ? true
+      : state.finishedLazyLoading,
+    items: action?.items ? action.items : state.items,
+  };
+}
+
+function setTagName(
+  updatedArgs: State["requestArgs"],
+  action: Action,
+  state: State
+) {
+  updatedArgs = {
+    ...updatedArgs,
+    newSearch: true,
+    page: 1,
+    query: "",
+    tagName: action?.tagName ? action.tagName : "",
+  };
+  return {
+    ...state,
+    finishedLazyLoading: false,
+    requestArgs: updatedArgs,
+  };
+}
+
+function setTags(state: State, action: Action): State {
+  return {
+    ...state,
+    galleryTags: action?.galleryTags || {},
+    items: action?.items?.length ? action.items : state.items,
+  };
+}
+
+function setSelectedItemComments(state: State, action: Action): State {
+  return {
+    ...state,
+    requestArgs: {
+      ...state["requestArgs"],
+      filter: false,
+      method: "",
+      selectedItemID: "",
+    },
+    selectedItem: getSelectedItem(
+      state.requestArgs.selectedItemID,
+      state.items
+    ),
+    selectedItemComments: action.selectedItemComments?.length
+      ? action.selectedItemComments
+      : state.selectedItemComments,
+  };
+}
+
+function setSearchRequestArgs(state: State, action: Action): State {
+  return {
+    ...state,
+    requestArgs: {
+      ...state["requestArgs"],
+      ...action.requestArgs,
+    },
+  };
+}
+
+function setRequestError(state: State): State {
+  return {
+    ...state,
+    items: [],
+    requestError: true,
+  };
+}
+
+function setIsLoading(state: State, action: Action): State {
+  return {
+    ...state,
+    isLoading: action?.loading !== undefined ? action.loading : state.isLoading,
+  };
+}
+
+function submitSearchRequest(
+  updatedArgs: State["requestArgs"],
+  action: Action,
+  state: State
+) {
+  const doneLoading = action.newSearch ? false : state.finishedLazyLoading;
+
+  updatedArgs = {
+    ...updatedArgs,
+    ...action,
+    tagName: "",
+  };
+
+  return {
+    ...state,
+    finishedLazyLoading: doneLoading,
+    requestArgs: updatedArgs,
+  };
+}
+
+/**
+ * The reducer function updates the state
+ * based on dispatched events
+ *
+ * It's used by the shared state
+ *
+ * TODO: Replace/refactor with better state management
+ *
+ * @param state
+ * @param action
+ */
+function stateReducer(state: State, action: Action): State {
+  const updatedArgs = {
+    ...state.requestArgs,
+  };
+
+  switch (action.type) {
+    case "setIsLoading":
+      return setIsLoading(state, action);
+
+    case "setItems":
+      return setItems(state, action);
+
+    case "selectedItemComments":
+      return setSelectedItemComments(state, action);
+
+    case "setSearchRequestArgs":
+      return setSearchRequestArgs(state, action);
+
+    case "setTagName":
+      return setTagName(updatedArgs, action, state);
+
+    case "setTags":
+      return setTags(state, action);
+
+    case "requestError":
+      return setRequestError(state);
+
+    case "submitSearchRequest":
+      return submitSearchRequest(updatedArgs, action, state);
+
+    default:
+      return state;
+  }
+}
+
+export { initialState, stateReducer };
---
+++ src/utils/types.ts
@@ -0,0 +1,79 @@
+type selectedTag = {
+  items?: Array<string>;
+};
+
+export type TypeGallery = {
+  description: string;
+  id: string;
+  name: string;
+};
+
+export type TypeTag = {
+  display_name: string;
+  name: string;
+  total_items?: number;
+};
+
+export type Item = {
+  account_url: string;
+  comment_count: number;
+  downs: number;
+  favorite_count: number;
+  id: string;
+  images: Array<{ height: number; link: string; type?: string; width: number }>;
+  title: string;
+  ups: number;
+  views: number;
+};
+
+export type Comments = {
+  author: string;
+  children: Array<Comments>;
+  comment: string;
+  datetime: number;
+  downs: number;
+  id: string;
+  ups: number;
+};
+
+export type SelectedComments = Array<Comments>;
+
+export type State = {
+  finishedLazyLoading: boolean;
+  galleryTags: { galleries?: []; tags?: [] };
+  isLoading: boolean;
+  items: Array<Item>;
+  requestArgs: {
+    filter: boolean;
+    method: string;
+    newSearch: boolean;
+    page: number;
+    query: string;
+    selectedItemID: string;
+    sort: string;
+    tagName: string;
+    window: string;
+  };
+  requestError: boolean;
+  selectedItem: Item | undefined;
+  selectedItemComments: SelectedComments;
+  selectedTag: selectedTag;
+};
+
+export type Action = {
+  finishedLazyLoading?: State["finishedLazyLoading"];
+  galleryTags?: State["galleryTags"];
+  items?: State["items"];
+  loading?: State["isLoading"];
+  method?: State["requestArgs"]["method"];
+  newSearch?: State["requestArgs"]["newSearch"];
+  page?: State["requestArgs"]["page"];
+  query?: State["requestArgs"]["query"];
+  requestArgs?: Partial<State["requestArgs"]>;
+  requestError?: boolean;
+  selectedItemComments?: State["selectedItemComments"];
+  sort?: State["requestArgs"]["sort"];
+  tagName?: State["requestArgs"]["tagName"];
+  type: string | null;
+  window?: State["requestArgs"]["window"];
+};
