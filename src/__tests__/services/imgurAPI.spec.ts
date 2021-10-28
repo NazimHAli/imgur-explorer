aa9eb495fa34cdb915b62dc50ebbef5cdd54db49
Nazim <nazimali@gmail.com>
2021-10-28 19:41:37 -0400

    MNT: More unit tests for imgurAPI

--- src/__tests__/services/imgurAPI.spec.ts
+++ src/__tests__/services/imgurAPI.spec.ts
@@ -5,8 +5,32 @@ describe("ImgurAPI", () => {
   const initialRequestArgs = initialState["requestArgs"];
   let api;
 
-  test("should init", () => {
+  test("request args should match", () => {
     api = ImgurAPI.getInstance(initialRequestArgs);
-    expect(api).toBeTruthy();
+    expect(api.requestArgs).toEqual(initialRequestArgs);
+  });
+
+  test("should use fake response", () => {
+    api = ImgurAPI.getInstance(initialRequestArgs);
+    expect(api.useFakeResponse).toBeTruthy();
+  });
+
+  test("should get gallery results", async () => {
+    api = ImgurAPI.getInstance(initialRequestArgs);
+    const res = await api.getGallerySearchResults();
+    expect(res.length).toEqual(60);
+  });
+
+  test("should get tags", async () => {
+    api = ImgurAPI.getInstance(initialRequestArgs);
+    const res = await api.methodDispatcher("tags");
+    expect(res.galleries.length).toEqual(4);
+    expect(res.tags.length).toEqual(88);
+  });
+
+  test.skip("dev", async () => {
+    api = ImgurAPI.getInstance(initialRequestArgs);
+    const res = await api.methodDispatcher("tags");
+    expect(res.length).toEqual(4);
   });
 });
