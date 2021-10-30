327daeaa5b7f25094d03b5cd0bb63d27c79a58b0
Nazim <nazimali@gmail.com>
2021-10-30 16:06:21 -0400

    ENH: Configure Sentry with Vercel deployments

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,20 @@
 import App from "@/components/App";
 import { GlobalContextProvider } from "@/state/GlobalContext";
 import "@/styles/index.scss";
+import * as Sentry from "@sentry/react";
+import { Integrations } from "@sentry/tracing";
 import { StrictMode } from "react";
 import { render } from "react-dom";
+
+Sentry.init({
+  dsn: "https://835823f448e64a669d1ebe74eaa9c0f9@o1056215.ingest.sentry.io/6042399",
+  integrations: [new Integrations.BrowserTracing()],
+
+  // Set tracesSampleRate to 1.0 to capture 100%
+  // of transactions for performance monitoring.
+  // We recommend adjusting this value in production
+  tracesSampleRate: 1.0,
+});
 
 render(
   <StrictMode>
