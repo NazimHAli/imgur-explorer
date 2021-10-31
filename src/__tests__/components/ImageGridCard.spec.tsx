f2eb67792b2c90ade9564f8a8878a9ce205e1985
Nazim <nazimali@gmail.com>
2021-10-31 11:36:24 -0400

    MNT: ImageGridCard.spec.tsx - test click scenarios

--- src/__tests__/components/ImageGridCard.spec.tsx
+++ src/__tests__/components/ImageGridCard.spec.tsx
@@ -1,45 +1,44 @@
 import { fakeResponse } from "@/__tests__/fixtures/imgurResponse";
 import ImageGridCard from "@/components/ImageGridCard";
-import { render, screen } from "@testing-library/react";
+import { render, screen, fireEvent } from "@testing-library/react";
+
+let testElement;
+
+const mockItem = fakeResponse.data[0];
+const item = {
+  account_url: mockItem.account_url,
+  comment_count: mockItem.comment_count,
+  downs: mockItem.downs,
+  favorite_count: mockItem.favorite_count,
+  id: mockItem.id,
+  images: [
+    {
+      height: mockItem.images[0].height,
+      link: `${mockItem.images[0].link}`,
+      width: mockItem.images[0].width,
+    },
+  ],
+  title: mockItem.title,
+  ups: mockItem.ups,
+  views: mockItem.views,
+};
+const mockSetArgs = jest.fn();
+
+function renderEle(isLoading = false) {
+  render(
+    <ImageGridCard
+      data-testid="card"
+      isLoading={isLoading}
+      item={item}
+      imgRef={() => {
+        null;
+      }}
+      setRequestArgs={mockSetArgs}
+    />
+  );
+}
 
 describe("ImageGridCard", () => {
-  let testElement;
-
-  const mockItem = fakeResponse.data[0];
-  const item = {
-    account_url: mockItem.account_url,
-    comment_count: mockItem.comment_count,
-    downs: mockItem.downs,
-    favorite_count: mockItem.favorite_count,
-    id: mockItem.id,
-    images: [
-      {
-        height: mockItem.images[0].height,
-        link: `${mockItem.images[0].link}`,
-        width: mockItem.images[0].width,
-      },
-    ],
-    title: mockItem.title,
-    ups: mockItem.ups,
-    views: mockItem.views,
-  };
-
-  function renderEle(isLoading = false) {
-    render(
-      <ImageGridCard
-        data-testid="card"
-        isLoading={isLoading}
-        item={item}
-        imgRef={() => {
-          null;
-        }}
-        setRequestArgs={(): void => {
-          null;
-        }}
-      />
-    );
-  }
-
   describe("loading skeleton", () => {
     test("render if isLoading=true", () => {
       renderEle(true);
@@ -77,5 +76,27 @@ describe("ImageGridCard", () => {
     testElement = document.querySelectorAll("svg");
 
     expect(testElement.length).toEqual(3);
+  });
+
+  test("clicking on card dispatches setRequestArgs", () => {
+    renderEle();
+
+    testElement = screen.getByRole("link");
+    fireEvent.click(testElement);
+
+    expect(mockSetArgs).toBeCalledTimes(1);
+    expect(mockSetArgs).toBeCalledWith({
+      filter: false,
+      method: "comments",
+      selectedItemID: item.id,
+    });
+
+    fireEvent.click(testElement);
+    expect(mockSetArgs).toBeCalledTimes(2);
+    expect(mockSetArgs).toBeCalledWith({
+      filter: false,
+      method: "comments",
+      selectedItemID: item.id,
+    });
   });
 });
