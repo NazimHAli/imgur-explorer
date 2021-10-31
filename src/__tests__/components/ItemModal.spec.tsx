05b87f22ad85b89110deeaa7d6ef85c5480393c9
Nazim <nazimali@gmail.com>
2021-10-31 17:46:21 -0400

    MNT: ItemModal.spec.tsx - test more scenarios

--- src/__tests__/components/ItemModal.spec.tsx
+++ src/__tests__/components/ItemModal.spec.tsx
@@ -1,23 +1,56 @@
 import { render, screen } from "@/__tests__/fixtures/test-utils";
 import ItemModal from "@/components/ItemModal";
+import "@testing-library/jest-dom";
+
+function renderModal({
+  isOpen = false,
+  setIsOpen = () => {
+    null;
+  },
+} = {}) {
+  render(<ItemModal isOpen={isOpen} setIsOpen={setIsOpen} />, {
+    container: document.getElementById("root"),
+  });
+}
 
 describe("ItemModal", () => {
-  beforeEach(() => {
-    render(
-      <ItemModal
-        isOpen={false}
-        setIsOpen={() => {
-          null;
-        }}
-      />,
-      {
-        container: document.getElementById("root"),
-      }
-    );
+  let testElement;
+
+  describe("renders one element when not open", () => {
+    renderModal();
+
+    test("should match snapshot", () => {
+      expect(document.querySelector("body")).toMatchSnapshot();
+    });
   });
 
-  test("mounts successfully", async () => {
-    screen.debug();
-    expect(document.querySelector(".ReactModalPortal")).toBeDefined();
+  describe("when open", () => {
+    beforeEach(() => {
+      renderModal({ isOpen: true });
+    });
+
+    test("mounts", () => {
+      expect(document.querySelector(".ReactModalPortal")).toBeDefined();
+    });
+
+    test("document.body contains modal open class", () => {
+      expect(document.querySelector("body").className).toBe(
+        "ReactModal__Body--open"
+      );
+    });
+
+    test("images not rendered", () => {
+      expect(screen.queryAllByRole("img")).toHaveLength(0);
+    });
+
+    test("has comments section", () => {
+      testElement = screen.getByRole("heading", { level: 3 });
+      expect(testElement).toHaveTextContent("Comments");
+    });
+
+    test.skip("dev", () => {
+      screen.debug();
+      expect(screen.queryAllByRole("img")).toHaveLength(0);
+    });
   });
 });
