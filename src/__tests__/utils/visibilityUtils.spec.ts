3764f23c35bdbe147db55b6035f52e3a91a21303
Nazim <nazimali@gmail.com>
2021-11-02 02:07:20 -0400

    MNT: Add new unit tests

--- src/__tests__/utils/visibilityUtils.spec.ts
+++ src/__tests__/utils/visibilityUtils.spec.ts
@@ -1,24 +1,27 @@
-import { isElementInView } from "@/utils/visibilityUtils";
+import {
+  ObserveElementsInView,
+  isElementInView,
+} from "@/utils/visibilityUtils";
+import { waitFor } from "@testing-library/dom";
+
+let testElement;
 
 function createElement(size: number) {
   document.body.innerHTML = "";
-  const testElement = document.createElement("div");
+  document.body.setAttribute("height", "1500px");
 
+  const testElement = document.createElement("div");
   testElement.setAttribute("width", `${size}px`);
   testElement.setAttribute("height", `${size}px`);
+  testElement.style.position = "absolute";
+  testElement.style.bottom = "0";
 
-  if (size === 0) {
-    testElement.style.position = "absolute";
-    testElement.style.bottom = "-100px";
-  }
+  document.body.appendChild(testElement);
 
-  document.body.append(testElement);
   return testElement;
 }
 
 describe("isElementInView", () => {
-  let testElement;
-
   test("false", () => {
     expect(isElementInView(null)).toBeFalsy();
   });
@@ -26,5 +29,35 @@ describe("isElementInView", () => {
   test("true", () => {
     testElement = createElement(20);
     expect(isElementInView(testElement)).toBeTruthy();
+  });
+});
+
+describe("ObserveElementsInView", () => {
+  let imgObserver, observe, unobserve;
+
+  beforeEach(() => {
+    observe = jest.fn();
+    unobserve = jest.fn();
+
+    // @ts-ignore
+    window.IntersectionObserver = jest.fn(() => ({
+      observe,
+      unobserve,
+    }));
+
+    imgObserver = new ObserveElementsInView();
+  });
+
+  test.skip("unobserve called", async () => {
+    testElement = createElement(20);
+    imgObserver.observeElements([testElement]);
+
+    window.scrollTo(0, document.body.scrollHeight);
+    await waitFor(() => expect(unobserve).toBeCalledTimes(1));
+  });
+
+  test("observe called", () => {
+    imgObserver.observeElements([createElement(20)]);
+    expect(observe).toBeCalledTimes(1);
   });
 });
